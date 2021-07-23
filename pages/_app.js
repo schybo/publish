56faf5dd58f88ddbf0bbaca31875a01ef4de65b4
Brent Scheibelhut <brent.scheibelhut@instacart.com>
2021-07-23 16:14:18 -0400

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import "../styles/globals.css";
+import React, { useEffect } from "react";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  id: "GTM-KJJZXBW",
+};
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
